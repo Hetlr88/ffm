331c8bef3d2c721a9c390834e6182448a6650786
Hetlr88 <mrhetlr0@gmail.com>
2024-08-23 23:45:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import asyncio
 import re
 from os import mkdir, system
 
-AppId=os.environ.get("API_ID")  #Replace With Your Own
-AppHash=os.environ.get("API_HASH") #Replace With Your Own
-BotToken=os.environ.get("BOT_TOKEN")  #Replace With Your Own
+AppId=os.environ.get("API_ID",29452145)  #Replace With Your Own
+AppHash=os.environ.get("API_HASH","5a2784e571fe5043852d32396a34a13b") #Replace With Your Own
+BotToken=os.environ.get("BOT_TOKEN","7445547849:AAHfX4_8TQ57cg71Q_l9ABoa6RfgfPWHgsE")  #Replace With Your Own
 app = Client("FFMpvdvdvdvegot", api_id=AppId, api_hash=AppHash, bot_token=BotToken)
 
 
